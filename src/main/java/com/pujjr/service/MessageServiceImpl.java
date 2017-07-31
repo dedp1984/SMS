eb00b583940c0ae8d0825e2b0aedaf6a2b767a2f
dedp1984 <154290629@qq.com>
2017-07-31 09:09:36 +0800

    添加日志记录

--- src/main/java/com/pujjr/service/MessageServiceImpl.java
+++ src/main/java/com/pujjr/service/MessageServiceImpl.java
@@ -4,6 +4,7 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,8 @@ import com.pujjr.message_api.service.IMessageService;
 @Service
 public class MessageServiceImpl implements IMessageService {
 
+	private static Logger log = Logger.getLogger(MessageServiceImpl.class); 
+	
 	@Autowired
 	private SmsWaitSendMapper smsWaitSendDao;
 	@Resource
@@ -31,7 +34,7 @@ public class MessageServiceImpl implements IMessageService {
 	@Override
 	public String sendSms(String sendId, String mobile, String content) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("sendIds");
+		log.info("----------------------------------------------开始调用短信发送接口-----------------------------------------");
 		try
 		 {
 			if(mobile.trim().length()!=11)
@@ -42,6 +45,7 @@ public class MessageServiceImpl implements IMessageService {
 			{
 				throw new Exception("电话号码存在非数字字符，请检查 ");
 			}
+			log.info("mobile = "+mobile+",content="+content);
 			SmsWaitSend smsWaitSend = new SmsWaitSend();
 			smsWaitSend.setId(Utils.get16UUID());
 			smsWaitSend.setSrcchnl("2");
@@ -54,6 +58,7 @@ public class MessageServiceImpl implements IMessageService {
 		 {
 			 throw new Exception(e.getMessage());
 		 }
+		log.info("----------------------------------------------结束调用短信发送接口-----------------------------------------");
 		return supplyName;
 	}
 
